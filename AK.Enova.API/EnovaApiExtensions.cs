ddf966a0f9fecb306fcb3d4117183f0e5ba9c796
fr3kz <tektmap1@gmail.com>
2026-02-01 18:08:08 +0100

    Add dll folder

--- AK.Enova.API/EnovaApiExtensions.cs
+++ AK.Enova.API/EnovaApiExtensions.cs
@@ -1,31 +1,65 @@
 ﻿using AK.Enova.API.Auth;
+using AK.Enova.API.Auth.AK.Enova.API;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AK.Enova.API
 {
     public static class EnovaApiExtensions
     {
         public static IServiceCollection AddEnovaApi(
-            this IServiceCollection services)
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
-            services.AddSingleton<EnovaService>();
+            services
+                .AddControllers()
+                .AddApplicationPart(typeof(EnovaService).Assembly);
+
+            services.Configure<EnovaConnectionOptions>(options =>
+            {
+                options.Database = configuration["Enova:Database"];
+                options.User = configuration["Enova:User"];
+                options.Password = configuration["Enova:Password"];
+
+                var authType = configuration["Enova:AuthType"];
+
+                options.AuthType =
+                    string.Equals(authType, "JWT", StringComparison.OrdinalIgnoreCase)
+                        ? EnovaAuthType.JWT
+                        : EnovaAuthType.None;
+            });
+
+            services.AddSingleton<IEnovaTokenStore>(
+                new SqliteTokenStore("enova_tokens.db"));
+
+            services.AddScoped<EnovaSessionFilter>();
+
+            services.AddSingleton<EnovaService>(sp =>
+            {
+                var opt = sp
+                    .GetRequiredService<IOptions<EnovaConnectionOptions>>()
+                    .Value;
+
+                return new EnovaService(opt);
+            });
+
             return services;
         }
 
         public static WebApplication UseEnovaApi(
             this WebApplication app)
         {
+            app.UseEnovaAuth();
             app.MapEnovaAuthEndpoints();
+            app.MapControllers();
             return app;
         }
+
         public static IApplicationBuilder UseEnovaAuth(
-       this IApplicationBuilder app)
+            this IApplicationBuilder app)
         {
             return app.UseMiddleware<EnovaJwtMiddleware>();
         }
--- AK.Enova.API/EnovaBootLoader.cs
+++ AK.Enova.API/EnovaBootLoader.cs
@@ -2,6 +2,7 @@
 using Soneta.Start;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,34 @@ namespace AK.Enova.API
             if (Loader.IsLoaded) return;
             if (_initialized) return;
 
+            var loader = new Soneta.Start.Loader() { WithUI = true, WithNet = false,WithExtensions=true };
+            
+            loader.UseCurrentDllsAsInstallation(true);
 
-            var loader = new Soneta.Start.Loader() { WithUI = true, WithNet = false,WithExtensions=true }; 
+            var baseDir = AppContext.BaseDirectory;
+            baseDir += @"dll\";
+
+            if (Path.Exists(baseDir))
+            {
+                var files = Directory.GetFiles(baseDir, "*.dll");
+
+                foreach (var file in files)
+                {
+                    var dllFullPath = Path.Combine(baseDir,file);
+
+                    if (File.Exists(dllFullPath))
+                    {
+                        loader.AddPrivateFileExtension(dllFullPath);
+                    }
+
+                }
+            }
+
+         
+
             loader.Load();
+
+            
 
 
             _initialized = true;
